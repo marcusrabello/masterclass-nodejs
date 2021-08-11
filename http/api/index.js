4b39fe89547f13ca1115b692dcc9fa3fe9674d99
Marcus Rabello <marcus.rabello@fourtime.com>
2021-08-11 20:50:37 -0300

    add api

---
+++ http/api/index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const data = require('./urls.json');
+
+function writeFile(callback){
+    fs.writeFile(
+        path.join(__dirname, "urls.json"), 
+        JSON.stringify(data, null, 2),
+        error => {
+            if(error) throw error
+
+            callback(JSON.stringify({message: "Conteudo deletado"}));
+        }
+    );
+}
+
+http.createServer((req, res) => {
+    const {name, url, del} = URL.parse(req.url, true).query;
+
+    res.writeHead(200, {
+        'Acces-Control-Allow-Origin': '*'
+    })
+
+    if(!name || !url)
+        return res.end(JSON.stringify(data));
+        
+    if(del){
+        data.urls = data.urls.filter(item => String(item.url) !== String(url));
+        return writeFile(message => res.end(message));
+    }
+        
+    data.urls.push({name, url});
+
+    return writeFile(message => res.end(message));
+
+}).listen(3000, () => console.log('API is running'));
